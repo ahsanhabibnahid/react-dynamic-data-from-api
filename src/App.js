5234d8adb19292a8ed86077799b1a44804253db3
Ahsan Habib Nahid <ahsanhabib3239@gmail.com>
2021-09-21 23:29:47 +0600

    dynamic data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  // dynamic data from api
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {
+          users.map(user => <Users name={user.name} email={user.email} key={user.id}></Users>)
+        }
+
       </header>
+    </div>
+  );
+}
+
+function Users(props) {
+  const { name, email } = props
+  return (
+    <div className="card text-dark p-5 m-3">
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{email}</p>
+      </div>
     </div>
   );
 }
